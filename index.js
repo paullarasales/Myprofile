00b7ee699d1c5b4ecde35ecdc44b864ce7f36078
Paul <ahmadzufukli@gmail.com>
2023-10-11 20:43:35 +0800

    portfolio

---
+++ index.js
@@ -0,0 +1,22 @@
+window.addEventListener('DOMContentLoaded', function() {
+    function scrollToView(sectionId) {
+        const section = document.querySelector(sectionId);
+        if (section) {
+            section.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    }
+
+    const experties = document.getElementById('experties');
+    experties.addEventListener('click', function(event) {
+        event.preventDefault();
+        scrollToView('.experties');
+    });
+
+    const services = document.getElementById('services');
+    services.addEventListener('click', function(event) {
+        event.preventDefault();
+        scrollToView('.skills')
+    })
+});
